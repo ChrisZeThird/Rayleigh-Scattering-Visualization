b6a91eb94ae9c0d73103228d574bfebc4ccaa1dc
Chris Ze Third <86256324+ChrisZeThird@users.noreply.github.com>
2022-09-13 16:50:03 +0200

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 13:54:01 2022
+
+@author: ChrisZeThird
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button
+
+import intersection as inter
+
+theta = np.linspace(start=0,stop=2*np.pi,num=100) # Angle to make the full circle
+
+""" Functions """
+
+def make_circle(r,theta):
+    x = np.cos(theta)*r
+    y = np.sin(theta)*r
+    return x,y
+
+def rotation(alpha):
+    return np.array([[np.cos(alpha),-np.sin(alpha)],[np.sin(alpha),np.cos(alpha)]])
+
+""" Parameters """
+
+r1 = 10
+r2 = 7
+
+## Light source 
+
+r = 12
+theta0 = np.pi/2
+x0, y0 = make_circle(r,theta0)
+
+## Observation point
+
+r_o = 12
+theta_o = np.pi
+x_o, y_o = make_circle(r_o,theta_o)
+
+## Planet + Atmosphere
+
+x1,y1 = make_circle(r1,theta) # atmosphere
+x2,y2 = make_circle(r2,theta) # planet
+
+""" Creating the lines between the original points """
+
+## making the lines
+a,b = inter.line(x0,y0,x_o,y_o)
+N = 5
+X0 = np.linspace(start=x0,stop=x_o,num=N,endpoint=True)
+line = a*X0 + b
+
+## finding the intersection
+inter1 = inter.intersection(x0,y0,x_o,y_o,r1) # crossing atmosphere
+inter2 = inter.intersection(x0,y0,x_o,y_o,r2) # crossing earth
+
+""" Ploting the values """
+
+## Figure settings
+
+val_max = max(r1,r2,r) + 5
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim([-val_max, val_max])
+ax.set_ylim([-val_max, val_max])
+
+plt.subplots_adjust(bottom=.35)
+
+ax.set_aspect('equal')
+
+## Ploting the figures
+
+ax.plot(x1,y1,c='skyblue') # atmosphere
+ax.plot(x2,y2,c='grey') # earth (center)
+
+t, = ax.plot(X0,line,c='grey',marker='o') # line between source and observation point
+p, = ax.plot(x0,y0,c='green', marker='o') # light source positioning
+q, = ax.plot(x_o,y_o,'bo') # observation point positioning
+
+
+dots1 = inter.dots_plot2(inter1,ax)
+dots2 = inter.dots_plot2(inter2,ax)
+
+## Creating sliders and button for the light source
+
+ax_theta = plt.axes([0.2,0.1,0.5,0.03])
+slider_theta = Slider(ax=ax_theta, label='Angle [rad]', valmin=0, valmax=2*np.pi, valinit=theta0, color='green')
+
+ax_radius = plt.axes([0.2,0.15,0.5,0.03])
+slider_radius = Slider(ax=ax_radius, label='Radius', valmin=10, valmax=val_max, valinit=r, color='green')
+    
+reset_button_ax_theta  = fig.add_axes([0.75, 0.1, 0.2, 0.04])
+reset_button_ax_radius = fig.add_axes([0.75, 0.15, 0.2, 0.04])
+reset_button_theta     = Button(reset_button_ax_theta, 'Reset Src. Angle', color='yellowgreen', hovercolor='green')
+reset_button_radius    = Button(reset_button_ax_radius, 'Reset Src. Radius',color='yellowgreen', hovercolor='green')
+
+def reset_button_on_clicked_theta(mouse_event):
+    slider_theta.reset()
+    
+def reset_button_on_clicked_radius(mouse_event):
+    slider_radius.reset()
+    
+reset_button_theta.on_clicked(reset_button_on_clicked_theta)
+reset_button_radius.on_clicked(reset_button_on_clicked_radius)
+
+## Creating sliders and buttons for the observation point
+
+ax_theta_o = plt.axes([0.2,0.2,0.5,0.03])
+slider_theta_o = Slider(ax=ax_theta_o, label='Angle [rad]', valmin=0, valmax=2*np.pi, valinit=theta_o)
+
+ax_radius_o = plt.axes([0.2,0.25,0.5,0.03])
+slider_radius_o = Slider(ax=ax_radius_o, label='Radius', valmin=10, valmax=15, valinit=r_o)
+
+reset_button_ax_theta_o  = fig.add_axes([0.75, 0.2, 0.2, 0.04])
+reset_button_ax_radius_o = fig.add_axes([0.75, 0.25, 0.2, 0.04])
+reset_button_theta_o     = Button(reset_button_ax_theta_o, 'Reset Obs. Angle', color='lightsteelblue', hovercolor='blue')
+reset_button_radius_o    = Button(reset_button_ax_radius_o, 'Reset Obs. Radius', color='lightsteelblue', hovercolor='blue')
+
+def reset_button_on_clicked_theta_o(mouse_event):
+    slider_theta_o.reset()
+    
+def reset_button_on_clicked_radius_o(mouse_event):
+    slider_radius_o.reset()
+    
+reset_button_theta_o.on_clicked(reset_button_on_clicked_theta_o)
+reset_button_radius_o.on_clicked(reset_button_on_clicked_radius_o)
+    
+
+## Update function common to all of the sliders
+
+def update(val):
+    current_theta = slider_theta.val
+    current_radius = slider_radius.val
+    
+    current_theta_o = slider_theta_o.val
+    current_radius_o = slider_radius_o.val
+    
+    x_prime,y_prime = make_circle(current_radius,current_theta) # new light source postion
+    p.set_xdata(x_prime)
+    p.set_ydata(y_prime)
+    
+    x_o_prime,y_o_prime = make_circle(current_radius_o,current_theta_o) # new light source postion
+    q.set_xdata(x_o_prime)
+    q.set_ydata(y_o_prime)
+    
+    A,B = inter.line(x_prime,y_prime,x_o_prime,y_o_prime)
+    X = np.linspace(start=x_prime,stop=x_o_prime,num=N,endpoint=True)
+    Line = A*X + B
+    t.set_xdata(X)
+    t.set_ydata(Line)
+    
+    global dots1
+    global dots2
+
+    
+    inter1 = inter.intersection(x_prime,y_prime,x_o_prime,y_o_prime,r1)
+    if dots1 is not None:
+        for l in dots1:
+            ax.lines.remove(l)
+    
+    
+    dots1 = inter.dots_plot2(inter1,ax)    
+        
+    inter2 = inter.intersection(x_prime,y_prime,x_o_prime,y_o_prime,r2)    
+    if dots2 is not None:
+        for l in dots2:
+            ax.lines.remove(l)
+    
+    dots2 = inter.dots_plot2(inter2,ax)    
+    if len(inter2)>0:
+        for l in dots2:
+            l.set_color('red')
+            # t.set_color('red')
+    else:
+        t.set_color('grey')
+        
+    
+    fig.canvas.draw_idle()
+
+slider_theta.on_changed(update)
+slider_radius.on_changed(update)
+
+slider_theta_o.on_changed(update)
+slider_radius_o.on_changed(update)
+
+plt.show()
---
+++ intersection.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 20:03:32 2022
+
+@author: ChrisZeThird
+
+Source: https://mathworld.wolfram.com/Circle-LineIntersection.html
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+## distance between two points
+def d(x1,x2):
+    return x2 - x1
+
+## norme of a vector depending on the distance between each coordinates
+def norme(d1,d2):
+    return np.sqrt(d1**2 + d2**2)
+
+def norme2(x1,x2,y1,y2):
+    dx = d(x1,x2)
+    dy = d(y1,y2)
+    return norme(dx,dy)
+
+## determinent
+def D(x1,y1,x2,y2):
+    return x1*y2 - x2*y1
+
+## sign function
+def sgn(x):
+    if x<0:
+        return -1
+    else:
+        return 1
+    
+## check if a point is on a line between two points
+def in_between(x1,y1,x2,y2,x,y):
+    """
+    Input: 
+        x1 -> float, x coordinate of the first point
+        y1 -> float, y coordinate of the first point
+        x2 -> float, x coordinate of the second point
+        y2 -> float, y coordinate of the second point
+        x -> float, x coordinate of the third point to check
+        y -> float, y coordinate of the third point to check
+        
+    Output:
+        Returns a tuple of the coefficients corresponding to the line equation (a,b): f(x)= ax + b
+    
+    Source: https://math.stackexchange.com/questions/701584/check-point-is-between-two-points
+        """
+        
+    a,b = line(x1,y1,x2,y2)
+    
+    if a*x + b != y:
+        return False
+    else:
+        # d0 = np.array([[d(x1,x2)],[d(y1,y2)]])
+        
+        # normd0 = np.linalg.norm(d0)     
+        # v = d0/normd0
+        # # print(v.shape)
+        
+        # d1 = np.array([[d(x,x1)],[d(y,y1)]])
+        # # print(d1.shape)
+        
+        # dot_product = np.dot(np.transpose(v),d1)
+        # # print(dot_product[0][0])
+        # return (0 <=dot_product[0][0] and dot_product[0][0] <= normd0)   
+        
+        bool1 = (x1 <= x <= x2) and (y1 <= y <= y2)
+        bool2 = (x1 <= x <= x2) and (y2 <= y <= y1)
+        bool3 = (x2 <= x <= x1) and (y2 <= y <= y1)
+        bool4 = (x2 <= x <= x1) and (y1 <= y <= y2)
+        return (bool1 or bool2 or bool3 or bool4)
+        
+## intersection between lines and circle
+def intersection(x1,y1,x2,y2,r):
+    """
+    Input: 
+        x1 -> float, x coordinate of the first point
+        y1 -> float, y coordinate of the first point
+        x2 -> float, x coordinate of the second point
+        y2 -> float, y coordinate of the second point
+        r -> float, radius of the circle
+    Output:
+        A list of coordinates indicating the intersection point(s)
+        between the line and the circle. If the result is None, there
+        is no intersection point.
+    """
+    # det = D(x1,y1,x2,y2)
+    # dx = d(x1,x2)
+    # dy = d(y1,y2)
+    # dr = norme(dx,dy)
+    # delta = (r**2 * dr**2) - det**2
+    # if delta>0:
+    #     X1 = (det*dy + np.sign(dy)*dx*np.sqrt(delta))/(dr**2)
+    #     Y1 = (-det*dx + abs(dy)*np.sqrt(delta))/(dr**2)
+    #     X2 = (det*dy - np.sign(dy)*dx*np.sqrt(delta))/(dr**2)
+    #     Y2 = (-det*dx - abs(dy)*np.sqrt(delta))/(dr**2)
+    #     return [(X1,Y1),(X2,Y2)]
+    # elif delta==0:
+    #     X_Prime = det*dy/(dr**2)
+    #     Y_Prime = -det*dx/(dr**2)
+    #     return [(X_Prime,Y_Prime)]
+    
+    a,b = line(x1,y1,x2,y2) # getting the coefficients of the line between the points
+    det = 4*(a**2)*(b**2) - 4*(a**2 + 1)*(b**2 - r**2)
+    
+    possible_intersections = []
+    
+    if det == 0:
+        X_prime = -a*b/(a**2 + 1)
+        Y_prime = a*(X_prime) + b
+        possible_intersections =  [(X_prime,Y_prime)]
+        # return [(X_prime,Y_prime)]
+    
+    elif det > 0:
+        X1 = (-2*a*b + np.sqrt(det))/(2*(a**2 +1))
+        Y1 = a*X1 + b
+        X2 = (-2*a*b - np.sqrt(det))/(2*(a**2 +1))
+        Y2 = a*X2 + b
+        possible_intersections = [(X1,Y1),(X2,Y2)]
+        # return [(X1,Y1),(X2,Y2)]
+    
+    if len(possible_intersections) > 0:
+        for element in possible_intersections[::-1]:
+            x,y = element
+            if in_between(x1, y1, x2, y2, x, y)==False:
+                possible_intersections.remove(element)
+
+        return possible_intersections    
+    
+    else:
+        return []
+
+
+def dots_plot2(intersect,ax):
+    if len(intersect) > 0:
+        # print(len(intersect))
+        u, = ax.plot(intersect[0][0],intersect[0][1],c='black',marker='o')
+        n = len(intersect)
+        if n == 1:
+            return u
+        elif n == 2:
+            v, = ax.plot(intersect[1][0],intersect[1][1],c='black',marker='o')
+            return u,v
+
+# def dots_remove(dots):
+#     if dots is not None:
+#         for k in range(len(dots)):
+#             l = dots[k]
+            
+#             dots[k] = l
+#         return dots
+
+## creating line between points
+def line(x1,y1,x2,y2):
+    """
+    Input: 
+        x1 -> float, x coordinate of the first point
+        y1 -> float, y coordinate of the first point
+        x2 -> float, x coordinate of the second point
+        y2 -> float, y coordinate of the second point
+    Output:
+        Returns a tuple of the coefficients corresponding to the line equation (a,b): f(x)= ax + b
+    """
+    return np.polyfit([x1,x2],[y1,y2],1)
+
+
+    
